e8f4c67fb2e536c11738622281693c126d317e48
Orkhan Mamedov <om@forcode.pro>
2022-08-18 09:53:10 +0300

    check if gitlab-runner is already running

--- local_gitlab_docker_executor.sh
+++ local_gitlab_docker_executor.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
-docker run -d \
-    –name gitlab-runner \
-    –restart never \
-    -v $PWD:$PWD \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    gitlab/gitlab-runner:latest \
+gitlab_pipeline_job_name=test
+image_name=gitlab/gitlab-runner:latest
+running_image=$(docker ps --format "{{.Image}}")
 
-docker exec -it -w $PWD gitlab-runner gitlab-runner exec docker test
+if [[ "${running_image}" != "${image_name}" ]]; then
+
+    docker run -d \
+        --name gitlab-runner \
+        --restart no \
+        -v $PWD:$PWD \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        $image_name
+
+fi
+
+docker exec -it -w $PWD gitlab-runner gitlab-runner exec docker $gitlab_pipeline_job_name
 
 #  Источник: второй ответ на https://stackoverflow.com/questions/32933174/use-gitlab-ci-to-run-tests-locally
 
